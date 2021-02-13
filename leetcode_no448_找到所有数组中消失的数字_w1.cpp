285fe5ac7429deeaff35c587d77539ce05bae5b5
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-13 13:44:33 +0800

    Add files via upload

---
+++ leetcode_no448_找到所有数组中消失的数字_w1.cpp
@@ -0,0 +1,81 @@
+﻿/* ------------------------------------------------------------------|
+给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+
+找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+
+您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+
+示例:
+
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[5,6]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：40 ms, 在所有 C++ 提交中击败了99.76%的用户
+*	内存消耗：31.4 MB, 在所有 C++ 提交中击败了82.64%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    int n = nums.size();
+    for (int& num : nums) {
+        int x = (num - 1) % n;
+        nums[x] += n;
+    }
+
+    vector<int> res;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= n) {
+            res.emplace_back(i + 1);
+        }
+    }
+    return res;
+}
